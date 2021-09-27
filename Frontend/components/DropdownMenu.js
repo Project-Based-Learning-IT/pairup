85640f35218eea0dee5397443ff988fe57ac56f2
siddheshkothadi <realsiddheshko@gmail.com>
2021-09-27 13:33:08 +0530

    replaced menu with popup modal

--- Frontend/components/DropdownMenu.js
+++ Frontend/components/DropdownMenu.js
@@ -1,40 +1,80 @@
 import React from 'react';
-import {TextInput, Menu} from 'react-native-paper';
+import {ScrollView, View} from 'react-native';
+import {Portal, Modal, Text} from 'react-native-paper';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import NewSection from './NewSection';
 
 function DropdownMenu({items, onChange, value, placeholder, label}) {
   const [visible, setVisible] = React.useState(false);
   const openMenu = () => setVisible(true);
   const closeMenu = () => setVisible(false);
 
+  const containerStyle = {backgroundColor: 'white', padding: 20};
+
   return (
-    <Menu
-      visible={visible}
-      onDismiss={closeMenu}
-      anchor={
-        <TextInput
-          mode="outlined"
-          label={label}
-          placeholder={placeholder}
-          value={value}
-          editable={false}
-          onPress={openMenu}
+    <View>
+      <View
+        style={{
+          borderWidth: 0.8,
+          borderColor: '#484848',
+          borderRadius: 6,
+          padding: 12,
+          paddingTop: 16,
+          paddingBottom: 16,
+          marginBottom: 20,
+          backgroundColor: '#fafafa',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <Text
           style={{
-            paddingBottom: 20,
+            fontSize: 16,
+            color: value === '' ? 'gray' : '#000',
+            flex: 1,
           }}
-          right={<TextInput.Icon name="menu-down" onPress={openMenu} />}
+          onPress={openMenu}>
+          {value === '' ? placeholder : value}
+        </Text>
+        <MaterialCommunityIcons
+          name="chevron-down"
+          size={24}
+          style={{marginLeft: 10, marginRight: 4}}
+          onPress={openMenu}
         />
-      }>
-      {items.map((item, index) => (
-        <Menu.Item
-          key={index}
-          title={item}
-          onPress={() => {
-            onChange(item);
-            closeMenu();
+      </View>
+      <Portal>
+        <Modal
+          style={{
+            padding: 40,
+            paddingLeft: 20,
+            paddingRight: 20,
           }}
-        />
-      ))}
-    </Menu>
+          visible={visible}
+          onDismiss={closeMenu}
+          contentContainerStyle={containerStyle}>
+          <ScrollView>
+            <NewSection name={placeholder} />
+            {items.map((item, index) => (
+              <Text
+                key={index}
+                style={{
+                  color: 'black',
+                  padding: 10,
+                  borderBottomColor: '#aaaaaa',
+                  borderBottomWidth: 0.8,
+                }}
+                onPress={() => {
+                  onChange(item);
+                  closeMenu();
+                }}>
+                {item}
+              </Text>
+            ))}
+          </ScrollView>
+        </Modal>
+      </Portal>
+    </View>
   );
 }
 export default DropdownMenu;
