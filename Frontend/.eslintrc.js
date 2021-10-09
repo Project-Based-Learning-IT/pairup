f2bcf797c6f305d0f05f7fa33a7360e2a55713c6
Sidhant Khamankar <avatarmeher999@gmail.com>
2021-10-10 00:32:29 +0530

    eslint prettier fight fix

--- Frontend/.eslintrc.js
+++ Frontend/.eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  // eslint prettier fight fix
+  parser: 'babel-eslint',
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  extends: ['plugin:prettier/recommended'],
 };
