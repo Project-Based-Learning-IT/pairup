ddd391a310524a6a908d1aa198aeb1e11451c912
Atharva Parikh <atharvaparikh@APs-Macbook-Air.local>
2021-10-14 11:35:23 +0530

    renamed the app to PairUp

--- Frontend/ios/PairUpTests/PairUpTests.m
+++ Frontend/ios/PairUpTests/PairUpTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface CollegeSpaceTests : XCTestCase
+@interface PairUpTests : XCTestCase
 
 @end
 
-@implementation CollegeSpaceTests
+@implementation PairUpTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
